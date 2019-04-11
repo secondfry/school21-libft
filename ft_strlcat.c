d4977a197726cef53a6eba47fbb7104b4e8faa7c
Rustam @Second_Fry Gubaydullin <secondfry@gmail.com>
2019-04-11 19:51:31 +0300

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -17,24 +17,18 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	dst_len;
 	size_t	src_len;
+	size_t	cpy_len;
 
 	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
-	// dst += dst_len;
-	// ft_memcpy()
-	// i = 0;
-	// q = 0;
-	// while (*dst++)
-	// 	i++;
-	// dst--;
-	// while (*src && i + q < size)
-	// 	*dst++ = *src++;
-	
-	// dst_len = ft_strlen(dst);
-	// src_len = ft_strlen(src);
-	// if (dst_len > size)
-	// 	return (dst_len + src_len);
-	// copy_len = size - dst_len;
-	// ft_strncpy(dst, src, copy_len);
-	return (dst_len + src_len + size - size);
+	cpy_len = src_len;
+	if (dst_len >= size)
+		return (size + src_len);
+	dst += dst_len;
+	size -= dst_len;
+	if (cpy_len >= size)
+		cpy_len = size - 1;
+	ft_memcpy(dst, src, cpy_len);
+	dst[cpy_len] = 0;
+	return (dst_len + src_len);
 }

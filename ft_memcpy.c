ec7d93e13fead1b892371a5f6631f626aef93e26
Rustam @Second_Fry Gubaydullin <secondfry@gmail.com>
2019-04-17 17:38:45 +0300

    revisited memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,19 +6,28 @@
 /*   By: oadhesiv <oadhesiv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/04 18:23:47 by oadhesiv          #+#    #+#             */
-/*   Updated: 2019/04/04 18:51:17 by oadhesiv         ###   ########.fr       */
+/*   Updated: 2019/04/17 17:37:28 by oadhesiv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
+#include "libft.h"
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	unsigned char		*dst_byte;
-	const unsigned char	*src_byte;
+	t_ulong			*dst_ulong;
+	const t_ulong	*src_ulong;
+	t_byte			*dst_byte;
+	const t_byte	*src_byte;
 
-	dst_byte = (unsigned char *)dst;
-	src_byte = (const unsigned char *)src;
+	dst_ulong = (t_ulong*)dst;
+	src_ulong = (const t_ulong*)src;
+	while (n > DATA_MODEL_LONG_WIDTH)
+	{
+		*dst_ulong++ = *src_ulong++;
+		n -= DATA_MODEL_LONG_WIDTH;
+	}
+	dst_byte = (t_byte*)dst_ulong;
+	src_byte = (const t_byte*)src_ulong;
 	while (n--)
 		*dst_byte++ = *src_byte++;
 	return (dst);

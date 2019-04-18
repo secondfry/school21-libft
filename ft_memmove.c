9b15f73e1d49b395235c758e7561bd9d288478c1
Rustam @Second_Fry Gubaydullin <oadhesiv@e8r9p8.21-school.ru>
2019-04-18 16:29:55 +0300

    revisited memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,22 +6,30 @@
 /*   By: oadhesiv <oadhesiv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/04 18:33:29 by oadhesiv          #+#    #+#             */
-/*   Updated: 2019/04/04 18:55:14 by oadhesiv         ###   ########.fr       */
+/*   Updated: 2019/04/18 16:27:13 by oadhesiv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
 #include "libft.h"
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	unsigned char		*dst_byte;
-	const unsigned char	*src_byte;
+	t_ulong			*dst_ulong;
+	const t_ulong	*src_ulong;
+	t_byte			*dst_byte;
+	const t_byte	*src_byte;
 
 	if (dst < src)
 		return (ft_memcpy(dst, src, len));
-	dst_byte = ((unsigned char *)dst) + len;
-	src_byte = ((const unsigned char *)src) + len;
+	dst_ulong = (t_ulong*)((t_byte*)dst + len);
+	src_ulong = (const t_ulong*)((t_byte*)src + len);
+	while (len > DATA_MODEL_LONG_WIDTH)
+	{
+		*--dst_ulong = *--src_ulong;
+		len -= DATA_MODEL_LONG_WIDTH;
+	}
+	dst_byte = (t_byte*)dst_ulong;
+	src_byte = (const t_byte*)src_ulong;
 	while (len--)
 		*--dst_byte = *--src_byte;
 	return (dst);

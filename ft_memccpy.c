d72fb7955a938b5de4e38db42376ce83920bd69b
Rustam @Second_Fry Gubaydullin <oadhesiv@e8r9p8.21-school.ru>
2019-04-18 17:17:19 +0300

    [!!! speed reduction !!!] start using of memchr in memccpy

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,49 +6,24 @@
 /*   By: oadhesiv <oadhesiv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/04 18:28:54 by oadhesiv          #+#    #+#             */
-/*   Updated: 2019/04/18 16:23:32 by oadhesiv         ###   ########.fr       */
+/*   Updated: 2019/04/18 17:16:03 by oadhesiv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static t_byte	check_long(const t_ulong *hay, t_byte needle)
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
-	t_byte	*hay_byte;
-	t_byte	i;
+	void	*ptr;
+	size_t	copy_len;
 
-	hay_byte = (t_byte*)hay;
-	i = 0;
-	while (i < DATA_MODEL_LONG_WIDTH)
+	ptr = ft_memchr(src, c, n);
+	if (ptr != (void*)0)
 	{
-		if (hay_byte[i] == needle)
-			return (0);
-		++i;
+		copy_len = ptr - src + 1;
+		ft_memcpy(dst, src, copy_len);
+		return (dst + copy_len);
 	}
-	return (1);
-}
-
-void			*ft_memccpy(void *dst, const void *src, int c, size_t n)
-{
-	t_ulong			*dst_ulong;
-	const t_ulong	*src_ulong;
-	t_byte			*dst_byte;
-	const t_byte	*src_byte;
-
-	dst_ulong = (t_ulong*)dst;
-	src_ulong = (const t_ulong*)src;
-	while (n > DATA_MODEL_LONG_WIDTH && check_long(src_ulong, (t_byte)c))
-	{
-		*dst_ulong++ = *src_ulong++;
-		n -= DATA_MODEL_LONG_WIDTH;
-	}
-	dst_byte = (t_byte*)dst_ulong;
-	src_byte = (t_byte*)src_ulong;
-	while (n--)
-	{
-		*dst_byte++ = *src_byte++;
-		if (*(src_byte - 1) == (t_byte)c)
-			return ((void*)dst_byte);
-	}
-	return (NULL);
+	ft_memcpy(dst, src, n);
+	return ((void*)0);
 }

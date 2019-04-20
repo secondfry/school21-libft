66e63bcceedae7b8780bbe1aa3ce5bf29a811a02
Rustam @Second_Fry Gubaydullin <oadhesiv@e8r9p8.21-school.ru>
2019-04-20 17:48:15 +0300

    revisited strncat

--- ft_strncat.c
+++ ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: oadhesiv <oadhesiv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/04 19:33:01 by oadhesiv          #+#    #+#             */
-/*   Updated: 2019/04/04 19:37:41 by oadhesiv         ###   ########.fr       */
+/*   Updated: 2019/04/20 17:47:28 by oadhesiv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,17 @@
 
 char	*ft_strncat(char *s1, const char *s2, size_t n)
 {
-	char	*ret;
+	size_t	s1_len;
+	size_t	s2_len;
+	size_t	copy_len;
 
-	ret = s1;
-	while (*s1++)
-		;
-	s1--;
-	while (*s2 && n--)
-		*s1++ = *s2++;
-	*s1 = 0;
-	return (ret);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	if (s2_len > n)
+		copy_len = n;
+	else
+		copy_len = s2_len;
+	ft_memcpy(s1 + s1_len, s2, copy_len);
+	*(s1 + s1_len + copy_len) = 0;
+	return (s1);
 }

f5be5b46d52cf40afef0e1b0c678cd569b7cd409
Rustam @Second_Fry Gubaydullin <oadhesiv@e8r9p8.21-school.ru>
2019-04-20 19:13:48 +0300

    revisited putstr

--- ft_putstr.c
+++ ft_putstr.c
@@ -6,14 +6,16 @@
 /*   By: oadhesiv <oadhesiv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/17 14:20:16 by oadhesiv          #+#    #+#             */
-/*   Updated: 2019/04/17 14:21:19 by oadhesiv         ###   ########.fr       */
+/*   Updated: 2019/04/20 19:13:10 by oadhesiv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include "libft.h"
 
 void	ft_putstr(char *str)
 {
-	while (*str)
-		write(1, str++, 1);
+	if (!str)
+		return ;
+	write(1, str, ft_strlen(str));
 }

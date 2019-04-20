a869c92ea86a3efb691c95f8bb4d1345fa95eb01
Rustam @Second_Fry Gubaydullin <oadhesiv@e8r9p8.21-school.ru>
2019-04-20 17:39:17 +0300

    revisited strcpy

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,17 +6,14 @@
 /*   By: oadhesiv <oadhesiv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/04 19:18:15 by oadhesiv          #+#    #+#             */
-/*   Updated: 2019/04/04 19:23:36 by oadhesiv         ###   ########.fr       */
+/*   Updated: 2019/04/20 17:37:29 by oadhesiv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_strcpy(char *dst, const char *src)
 {
-	char	*ret;
-
-	ret = dst;
-	while (*src)
-		*dst++ = *src++;
-	*dst = 0;
-	return (ret);
+	ft_memcpy(dst, src, ft_strlen(src) + 1);
+	return (dst);
 }

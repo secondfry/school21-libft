2f1062f2f3573c7c35496c5f241eb0ef80a0a9dc
Rustam @Second_Fry Gubaydullin <secondfry@gmail.com>
2019-04-23 16:52:00 +0300

    fix Windows ^M nonsense

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: oadhesiv <oadhesiv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/23 15:46:18 by oadhesiv          #+#    #+#             */
-/*   Updated: 2019/04/23 16:25:47 by oadhesiv         ###   ########.fr       */
+/*   Updated: 2019/04/23 16:51:18 by oadhesiv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ void	*ft_memalloc(size_t size)
 	void	*ret;
 
 	ret = malloc(size);
-  if (!ret)
-    return ((void *)0);
+	if (!ret)
+		return ((void *)0);
 	ft_bzero(ret, size);
 	return (ret);
 }
--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,14 +6,14 @@
 /*   By: oadhesiv <oadhesiv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/23 15:16:37 by oadhesiv          #+#    #+#             */
-/*   Updated: 2019/04/23 15:17:16 by oadhesiv         ###   ########.fr       */
+/*   Updated: 2019/04/23 16:51:02 by oadhesiv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void  ft_putendl_fd(char const *s, int fd)
+void	ft_putendl_fd(char const *s, int fd)
 {
-  ft_putstr_fd(s, fd);
-  ft_putchar_fd('\n', fd);
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
 }
